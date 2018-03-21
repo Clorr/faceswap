d4321422770a017dc112d736b8f130b6b69ecb75
Clorr <github@clorr.fr>
2018-03-21 18:20:19 +0100

    Grouped convolutions

--- plugins/Model_OriginalRetrain/Model.py
+++ plugins/Model_OriginalRetrain/Model.py
@@ -1,7 +1,7 @@
 # Based on the original https://www.reddit.com/r/deepfakes/ code sample + contribs
 
 from keras.models import Model as KerasModel
-from keras.layers import Input, Dense, Flatten, Reshape, add, concatenate
+from keras.layers import Input, Dense, Flatten, Reshape, BatchNormalization, Lambda, MaxPool2D, add, concatenate
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import Conv2D
 from keras.optimizers import Adam
@@ -12,6 +12,8 @@ from lib.PixelShuffler import PixelShuffler
 IMAGE_SHAPE = (64, 64, 3)
 ENCODER_DIM = 1024
 
+cardinality = 4
+
 class Model(AutoEncoder):
     def initModel(self):
         optimizer = Adam(lr=5e-5, beta_1=0.5, beta_2=0.999)
@@ -20,7 +22,7 @@ class Model(AutoEncoder):
         encoder = self.encoder
         encoder.trainable = False
         decoder = self.decoder_B
-        #decoder.trainable = False
+        decoder.trainable = False
         self.autoencoder_B = KerasModel(x, self.refiner(decoder(encoder(x))))
 
         self.autoencoder_B.compile(optimizer=optimizer, loss='mean_absolute_error')
@@ -81,4 +83,71 @@ class Model(AutoEncoder):
     def Refiner(self):
         input_ = Input(shape=(256, 256, 3))
         x = input_
+        for i in range(3):
+            project_shortcut = True if i == 0 else False
+            x = self.residual_block(x, 16, 3, _project_shortcut=project_shortcut)
         return KerasModel(input_, x)
+
+#From https://blog.waya.ai/deep-residual-learning-9610bb62c355
+
+    def add_common_layers(self, y):
+        #y = BatchNormalization()(y)
+        y = LeakyReLU()(y)
+
+        return y
+
+    def grouped_convolution(self, y, nb_channels, _strides):
+        # when `cardinality` == 1 this is just a standard convolution
+        if cardinality == 1:
+            return Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
+        
+        assert not nb_channels % cardinality
+        _d = nb_channels // cardinality
+
+        # in a grouped convolution layer, input and output channels are divided into `cardinality` groups,
+        # and convolutions are separately performed within each group
+        groups = []
+        for j in range(cardinality):
+            group = Lambda(lambda z: z[:, :, :, j * _d:j * _d + _d])(y)
+            groups.append(Conv2D(_d, kernel_size=(3, 3), strides=_strides, padding='same')(group))
+            
+        # the grouped convolutional layer concatenates them as the outputs of the layer
+        y = concatenate(groups)
+
+        return y
+
+    def residual_block(self, y, nb_channels_in, nb_channels_out, _strides=(1, 1), _project_shortcut=False):
+        """
+        Our network consists of a stack of residual blocks. These blocks have the same topology,
+        and are subject to two simple rules:
+        - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
+        - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
+        """
+        shortcut = y
+
+        # we modify the residual building block as a bottleneck design to make the network more economical
+        y = Conv2D(nb_channels_in, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+        y = self.add_common_layers(y)
+
+        # ResNeXt (identical to ResNet when `cardinality` == 1)
+        y = self.grouped_convolution(y, nb_channels_in, _strides=_strides)
+        y = self.add_common_layers(y)
+
+        y = Conv2D(nb_channels_out, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
+        # batch normalization is employed after aggregating the transformations and before adding to the shortcut
+        y = BatchNormalization()(y)
+
+        # identity shortcuts used directly when the input and output are of the same dimensions
+        if _project_shortcut or _strides != (1, 1):
+            # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
+            # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
+            shortcut = Conv2D(nb_channels_out, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
+            shortcut = BatchNormalization()(shortcut)
+
+        y = add([shortcut, y])
+
+        # relu is performed right after each batch normalization,
+        # expect for the output of the block where relu is performed after the adding to the shortcut
+        y = LeakyReLU()(y)
+
+        return y
