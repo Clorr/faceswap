868f0b7b1eacbd951d79ccc38a203964649bd0fc
Clorr <github@clorr.fr>
2018-03-22 14:55:17 +0100

    Test SRCNN

--- plugins/Model_OriginalRetrain/Model.py
+++ plugins/Model_OriginalRetrain/Model.py
@@ -3,8 +3,10 @@
 from keras.models import Model as KerasModel
 from keras.layers import Input, Dense, Flatten, Reshape, BatchNormalization, Lambda, MaxPool2D, add, concatenate
 from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.initializers import RandomNormal
 from keras.optimizers import Adam
+from keras import backend as K
 
 from .AutoEncoder import AutoEncoder
 from lib.PixelShuffler import PixelShuffler
@@ -12,7 +14,22 @@ from lib.PixelShuffler import PixelShuffler
 IMAGE_SHAPE = (64, 64, 3)
 ENCODER_DIM = 1024
 
-cardinality = 4
+conv_init = RandomNormal(0, 0.02)
+
+def ground_truth_diff(y_true, y_pred):
+    return K.abs(y_pred - y_true)
+
+#from https://github.com/YapengTian/SRCNN-Keras/blob/master/SRCNN_train/SRCNN_train.py
+def PSNRLoss(y_true, y_pred):
+    """
+    PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
+    It can be calculated as
+    PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
+    When providing an unscaled input, MAXp = 255. Therefore 20 * log10(255)== 48.1308036087.
+    However, since we are scaling our input, MAXp = 1. Therefore 20 * log10(1) = 0.
+    Thus we remove that component completely and only compute the remaining MSE component.
+    """
+    return -10. * np.log10(K.mean(K.square(y_pred - y_true)))
 
 class Model(AutoEncoder):
     def initModel(self):
@@ -81,73 +98,21 @@ class Model(AutoEncoder):
         return KerasModel(input_, x)
 
     def Refiner(self):
+        
+        def srcnn(x, channels):
+            x = Conv2D(64, 9, padding='same', activation='relu', init='he_normal')(x)
+            x = Conv2D(32, 1, padding='same', activation='relu', init='he_normal')(x)
+            x = Conv2D(channels, 5, padding='same', init='he_normal')(x)
+            return x
+        
         input_ = Input(shape=(256, 256, 3))
         x = input_
-        for i in range(3):
-            project_shortcut = True if i == 0 else False
-            x = self.residual_block(x, 16, 3, _project_shortcut=project_shortcut)
+        # x = Lambda(lambda image: K.tf.image.rgb_to_hsv(image))(x)
+        # Y = Lambda(lambda x : x[:,:,:,:1])(x)
+        # Cr = Lambda(lambda x : x[:,:,:,1:2])(x)
+        # Cb = Lambda(lambda x : x[:,:,:,2:3])(x)
+        # Y = srcnn(Y, 1)
+        # x = concatenate([Y, Cr, Cb])
+        # x = Lambda(lambda image: K.tf.image.hsv_to_rgb(image))(x) #Fails with 'None value are not supported'
+        x = srcnn(x, 3)
         return KerasModel(input_, x)
-
-#From https://blog.waya.ai/deep-residual-learning-9610bb62c355
-
-    def add_common_layers(self, y):
-        #y = BatchNormalization()(y)
-        y = LeakyReLU()(y)
-
-        return y
-
-    def grouped_convolution(self, y, nb_channels, _strides):
-        # when `cardinality` == 1 this is just a standard convolution
-        if cardinality == 1:
-            return Conv2D(nb_channels, kernel_size=(3, 3), strides=_strides, padding='same')(y)
-        
-        assert not nb_channels % cardinality
-        _d = nb_channels // cardinality
-
-        # in a grouped convolution layer, input and output channels are divided into `cardinality` groups,
-        # and convolutions are separately performed within each group
-        groups = []
-        for j in range(cardinality):
-            group = Lambda(lambda z: z[:, :, :, j * _d:j * _d + _d])(y)
-            groups.append(Conv2D(_d, kernel_size=(3, 3), strides=_strides, padding='same')(group))
-            
-        # the grouped convolutional layer concatenates them as the outputs of the layer
-        y = concatenate(groups)
-
-        return y
-
-    def residual_block(self, y, nb_channels_in, nb_channels_out, _strides=(1, 1), _project_shortcut=False):
-        """
-        Our network consists of a stack of residual blocks. These blocks have the same topology,
-        and are subject to two simple rules:
-        - If producing spatial maps of the same size, the blocks share the same hyper-parameters (width and filter sizes).
-        - Each time the spatial map is down-sampled by a factor of 2, the width of the blocks is multiplied by a factor of 2.
-        """
-        shortcut = y
-
-        # we modify the residual building block as a bottleneck design to make the network more economical
-        y = Conv2D(nb_channels_in, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
-        y = self.add_common_layers(y)
-
-        # ResNeXt (identical to ResNet when `cardinality` == 1)
-        y = self.grouped_convolution(y, nb_channels_in, _strides=_strides)
-        y = self.add_common_layers(y)
-
-        y = Conv2D(nb_channels_out, kernel_size=(1, 1), strides=(1, 1), padding='same')(y)
-        # batch normalization is employed after aggregating the transformations and before adding to the shortcut
-        y = BatchNormalization()(y)
-
-        # identity shortcuts used directly when the input and output are of the same dimensions
-        if _project_shortcut or _strides != (1, 1):
-            # when the dimensions increase projection shortcut is used to match dimensions (done by 1×1 convolutions)
-            # when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2
-            shortcut = Conv2D(nb_channels_out, kernel_size=(1, 1), strides=_strides, padding='same')(shortcut)
-            shortcut = BatchNormalization()(shortcut)
-
-        y = add([shortcut, y])
-
-        # relu is performed right after each batch normalization,
-        # expect for the output of the block where relu is performed after the adding to the shortcut
-        y = LeakyReLU()(y)
-
-        return y
