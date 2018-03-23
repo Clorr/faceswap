68e1ba772206208df9e010f58a09e4136b5667e5
Clorr <github@clorr.fr>
2018-03-23 19:55:32 +0100

    Test DCSCN

--- plugins/Model_OriginalRetrain/Model.py
+++ plugins/Model_OriginalRetrain/Model.py
@@ -29,7 +29,20 @@ def PSNRLoss(y_true, y_pred):
     However, since we are scaling our input, MAXp = 1. Therefore 20 * log10(1) = 0.
     Thus we remove that component completely and only compute the remaining MSE component.
     """
-    return -10. * np.log10(K.mean(K.square(y_pred - y_true)))
+    return -10. * log10(K.mean(K.square(y_pred - y_true)))
+
+def log10(x):
+    numerator = K.tf.log(x)
+    denominator = K.tf.log(K.tf.constant(10, dtype=numerator.dtype))
+    return numerator / denominator
+
+# #from https://github.com/mkocabas/focal-loss-keras
+# def focal_loss(gamma=2., alpha=.25):
+#     def focal_loss_fixed(y_true, y_pred):
+#         pt_1 = K.tf.where( K.tf.equal(y_true, 1), y_pred, K.tf.ones_like(y_pred))
+#         pt_0 = K.tf.where( K.tf.equal(y_true, 0), y_pred, K.tf.zeros_like(y_pred))
+#         return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+#     return focal_loss_fixed
 
 class Model(AutoEncoder):
     def initModel(self):
@@ -42,7 +55,7 @@ class Model(AutoEncoder):
         decoder.trainable = False
         self.autoencoder_B = KerasModel(x, self.refiner(decoder(encoder(x))))
 
-        self.autoencoder_B.compile(optimizer=optimizer, loss='mean_absolute_error')
+        self.autoencoder_B.compile(optimizer=optimizer, loss='mse')
 
         encoder.summary()
         decoder.summary()
@@ -98,21 +111,35 @@ class Model(AutoEncoder):
         return KerasModel(input_, x)
 
     def Refiner(self):
-        
-        def srcnn(x, channels):
-            x = Conv2D(64, 9, padding='same', activation='relu', init='he_normal')(x)
-            x = Conv2D(32, 1, padding='same', activation='relu', init='he_normal')(x)
-            x = Conv2D(channels, 5, padding='same', init='he_normal')(x)
-            return x
+        #based on DCSCN https://arxiv.org/abs/1707.05425
+        def feat(filters):
+            def block(x):
+                x = Conv2D(filters, kernel_size=3, padding='same')(x)
+                x = LeakyReLU(0.1)(x)
+                return x
+            return block
+
+        def recon(filters, k):
+            def block(x):
+                x = Conv2D(filters, kernel_size=k, padding='same')(x)
+                x = LeakyReLU(0.1)(x)
+                return x
+            return block
         
         input_ = Input(shape=(256, 256, 3))
         x = input_
-        # x = Lambda(lambda image: K.tf.image.rgb_to_hsv(image))(x)
-        # Y = Lambda(lambda x : x[:,:,:,:1])(x)
-        # Cr = Lambda(lambda x : x[:,:,:,1:2])(x)
-        # Cb = Lambda(lambda x : x[:,:,:,2:3])(x)
-        # Y = srcnn(Y, 1)
-        # x = concatenate([Y, Cr, Cb])
-        # x = Lambda(lambda image: K.tf.image.hsv_to_rgb(image))(x) #Fails with 'None value are not supported'
-        x = srcnn(x, 3)
+        f1 = feat(96)(x)
+        f2 = feat(76)(f1)
+        f3 = feat(65)(f2)
+        f4 = feat(55)(f3)
+        f5 = feat(47)(f4)
+        f6 = feat(39)(f5)
+        f7 = feat(32)(f6)
+        x = concatenate([f1, f2, f3, f4, f5, f6, f7], axis=3)
+        a1 = recon(64, 3)(x)
+        b1 = recon(32, 1)(x)
+        b2 = recon(32, 3)(b1)
+        x = concatenate([a1, b2])
+        x = Conv2D(3, kernel_size=1, padding='same', activation='sigmoid')(x)
+        x = add([x, input_])
         return KerasModel(input_, x)
